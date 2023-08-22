a663148a09ea61496c07ca648f2644bed844b24a
Karan Jadaun <jadaunfamily6@gmail.com>
2023-08-22 11:26:50 +0530

    Create 3Sum.py

---
+++ Array/3Sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        n = len(nums)
+        for i in range(n):
+            temp = nums[i]
+            left = i + 1
+            right = n - 1
+            while (left < right):
+                s = temp + nums[left] + nums[right]
+                if (s > 0):
+                    right -= 1
+                elif (s < 0):
+                    left += 1
+                else:
+                    val = [temp, nums[left], nums[right]]
+                    if (val not in res):
+                        res.append(val)
+                    left += 1
+                    right -= 1
+        return res
