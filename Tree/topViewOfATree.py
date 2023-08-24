7bc33f520f275d94af6e31b8ec0b6b66aff7d3a2
Karan Jadaun <jadaunfamily6@gmail.com>
2023-08-24 10:59:16 +0530

    Create topViewOfATree.py

---
+++ Tree/topViewOfATree.py
@@ -0,0 +1,15 @@
+class Solution:
+    def topView(self,root):
+        hashmap = {}
+        queue = [(root, 0)]
+        while (queue):
+            root, level = queue.pop(0)
+            if (level not in hashmap):
+                hashmap[level] = root.data
+            if (root.left): queue.append((root.left, level - 1))
+            if (root.right): queue.append((root.right, level + 1))
+        arr = []
+        for i in sorted(hashmap.keys()):
+            arr.append(hashmap[i])
+        return arr
+        
