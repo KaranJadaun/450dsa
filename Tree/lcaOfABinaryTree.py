da7c9efd6e04c80994239857de145f629e707314
Karan Jadaun <jadaunfamily6@gmail.com>
2023-08-24 11:04:28 +0530

    Create lcaOfABinaryTree.py

---
+++ Tree/lcaOfABinaryTree.py
@@ -0,0 +1,9 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None or p is root or q is root: 
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left is None: return right
+        elif right is None: return left
+        return root
